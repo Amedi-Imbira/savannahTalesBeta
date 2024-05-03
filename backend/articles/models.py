1748423c6d8077199f35cc9a0e5e215b4d4d4cf7
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-05-03 15:31:16 +0300

    ✨Created Article model + applied migrations

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Article(models.Model):
+      
+      class Status(models.TextChoices):
+            DRAFT = 'DF', 'Drafted'
+            PUBLISHED = 'PB', 'Published'
+      
+      title = models.CharField(max_length=200)
+      slug = models.SlugField(max_length=250)
+      body = models.TextField()
+      status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+      author = models.ForeignKey(User, on_delete=models.CASCADE)
+      published_on = models.DateTimeField(default=timezone.now)
+      created_at = models.DateTimeField(auto_now_add=True)
+      edited = models.DateTimeField(auto_now=True)
+      
+      class Meta:
+            ordering = ['-published_on']
+            indexes = [
+                  models.Index(fields=['-published_on']),
+            ]
+      
+      def __str__(self):
+            return self.title
 
 # Create your models here.
