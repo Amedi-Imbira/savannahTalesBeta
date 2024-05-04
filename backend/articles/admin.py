4834705735de08161904624d976ad84bc6c4cc21
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-05-04 16:35:21 +0300

    Added Vue to the project

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+      list_display = ['id', 'title', 'body', 'author', 'published_on', 'status']
+      prepopulated_fields = {'slug': ('title',)}
+
 # Register your models here.
--- backend/articles/views.py
+++ backend/articles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleList(APIView):
+      def get(self, request):
+            published_articles = Article.objects.filter(status=Article.Status.PUBLISHED)
+            serializer = ArticleSerializer(published_articles, many=True)
+            return Response(serializer.data)
+      
+      def post(self, request):
+            serializer = ArticleSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            print(serializer.validated_data)
+            return Response('OK')
