fd9336b27066d7e9b270126eb7f9be87c49d9a91
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-05-04 17:38:22 +0300

    Able to read articles from the vue

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     # Third-party apps
     'rest_framework',
+    'corsheaders',
      
     # Local apps
     'articles',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -127,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
 import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
 
+// Vue.prototype.$http = axios;
+
 const app = createApp(App)
+
+
 
 app.use(createPinia())
 app.use(router)
